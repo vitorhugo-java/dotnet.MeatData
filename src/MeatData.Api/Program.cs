3a189b67f321f0a1ff94bcc5789ce63ab72e2754
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 22:19:11 -0300

    feat: connectstring and DI EF

--- src/MeatData.Api/Program.cs
+++ src/MeatData.Api/Program.cs
@@ -1,3 +1,6 @@
+using MeatData.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
