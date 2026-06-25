4d1ab78f1369a177de2b658993c392b6e7c2791d
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:12:08 -0300

    feat: create NutritionProfile

---
+++ src/MeatData.Domain/Entities/NutritionProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Domain.Entities
+{
+    public class NutritionProfile
+    {
+        Guid Id { get; set; }
+        Guid ProductId { get; set; }
+        string FdclId { get; set; }
+        decimal Calories { get; set; }
+        decimal ProteinGrams { get; set; }
+        decimal FatGrams { get; set; }
+        decimal CarbsGrams { get; set; }
+        decimal SodiumMg { get; set; }
+        DateTime FetchedAt { get; set; }
+        string Source { get; set; }
+
+        private NutritionProfile() { }
+    }
+}
