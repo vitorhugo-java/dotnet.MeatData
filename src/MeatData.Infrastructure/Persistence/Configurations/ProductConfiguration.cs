07043ed8fe27a4f3671c110e5a383a652aa81c98
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:56:02 -0300

    feat: create ProductConfiguration

---
+++ src/MeatData.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using MeatData.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).HasMaxLength(500);
+            builder.Property(x => x.SKU).HasMaxLength(50).IsRequired();
+            builder.HasIndex(p => p.SKU).IsUnique();
+            builder.Property(p => p.WeightGrams).HasPrecision(10, 2);
+            builder.HasOne(p => p.Category)
+                   .WithMany(c => c.Product)
+                   .HasForeignKey(p => p.CategoryId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
