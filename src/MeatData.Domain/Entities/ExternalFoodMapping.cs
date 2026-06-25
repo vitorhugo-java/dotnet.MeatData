29fbdbabfd01614a312726332c5f8fd4a2134459
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:40:35 -0300

    faet: add ExternalFoodMapping entity

---
+++ src/MeatData.Domain/Entities/ExternalFoodMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Domain.Entities
+{
+    public class ExternalFoodMapping
+    {
+        Guid Id { get; set; }
+        Guid ProductId { get; set; }
+        string ExternalId { get; set; }
+        string ExternalSource { get; set; }
+        string SearchQuery { get; set; }
+        DateTime MappedAt { get; set; }
+
+        private ExternalFoodMapping() { }
+    }
+}
