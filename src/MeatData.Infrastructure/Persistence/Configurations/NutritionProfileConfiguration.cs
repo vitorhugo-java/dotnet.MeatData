3140f425b3a738f1e777c82c26bc8db23bdd137d
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 22:03:09 -0300

    feat: create NutritionProfileConfiguration

---
+++ src/MeatData.Infrastructure/Persistence/Configurations/NutritionProfileConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using MeatData.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Infrastructure.Persistence.Configurations
+{
+    public class NutritionProfileConfiguration : IEntityTypeConfiguration<NutritionProfile>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<NutritionProfile> builder)
+        {
+            builder.HasKey(np => np.Id);
+            builder.Property(np => np.FdclId).IsRequired().HasMaxLength(50);
+            builder.Property(np => np.Calories).HasPrecision(10, 2);
+            builder.Property(np => np.ProteinGrams).HasPrecision(10, 2);
+            builder.Property(np => np.FatGrams).HasPrecision(10, 2);
+            builder.Property(np => np.CarbsGrams).HasPrecision(10, 2);
+            builder.Property(np => np.SodiumMg).HasPrecision(10, 2);
+            builder.Property(np => np.Source).IsRequired().HasMaxLength(100);
+            builder.HasOne<Product>()
+                   .WithMany()
+                   .HasForeignKey(np => np.ProductId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
