d9399527e65a8707e244588443c996e74360cd71
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:16:06 -0300

    feat: create ExternalFoodMapping

---
+++ src/MeatData.Domain/Entities/ExternalFoodMapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Domain.Entities
+{
+    public class ExternalFoodMapping
+    {
+        Guid Id { get; set; }
+        Guid ProductId { get; set; }
+        string ApiName { get; set; }
+        string Endpoint { get; set; }
+        int StatusCode { get; set; }
+        bool CacheHit { get; set; }
+        int DurationMs { get; set; }
+        DateTime RequestedAt { get; set; }
+    }
+}
