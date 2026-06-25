ac0599a4a2c8543d96e071432a164b8ac51da2f8
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:03:10 -0300

    feat: create productcategory

---
+++ src/MeatData.Domain/Entities/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using MeatData.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Domain.Entities
+{
+    public class ProductCategory
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        AnimalCategory animalCategory { get; set; }
+        DateTime CreatedAt { get; set; }
+
+    }
+}
