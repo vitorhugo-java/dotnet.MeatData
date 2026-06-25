c1368d7db257127b9be36af470da92563bb1cf05
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:43:14 -0300

    feat: add AppDbContext setting for EF

---
+++ src/MeatData.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MeatData.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<ProductCategory> Categories => Set<ProductCategory>();
+    public DbSet<NutritionProfile> NutritionProfiles => Set<NutritionProfile>();
+    public DbSet<ExternalFoodMapping> ExternalFoodMappings => Set<ExternalFoodMapping>();
+    public DbSet<ExternalApiRequestLog> ExternalApiRequestLogs => Set<ExternalApiRequestLog>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
