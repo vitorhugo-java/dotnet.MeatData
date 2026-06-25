3707b82d9cbf9401193eb11179202cf1f7c6ea94
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-24 21:02:56 -0300

    feat: add AnimalCategory

---
+++ src/MeatData.Domain/Enums/AnimalCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Domain.Enums
+{
+    public enum AnimalCategory
+    {
+        Bovino = 1,
+        Suino = 2,
+        Aves = 3,
+        Ovino = 4,
+        Caprino = 5,
+        Bubalino = 6,
+        Pescados = 7,
+        FrutosDoMar = 8,
+        Caca = 9,
+        Processados = 10,
+        Outros = 99
+    }
+}
