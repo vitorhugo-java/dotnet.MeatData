874617113a42b899688949ea13c163af058f4e9e
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-06-28 23:06:53 -0300

    feat: create Product command

---
+++ src/MeatData.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Application.Products.Commands.CreateProduct
+{
+    public record CreateProductCommand(
+    string Name,
+    string? Description,
+    string SKU,
+    decimal WeightGrams,
+    Guid CategoryId);
+}
---
+++ src/MeatData.Application/Products/Commands/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using MeatData.Application.Common;
+using MeatData.Application.Interfaces;
+using MeatData.Application.Interfaces.Repositories;
+using MeatData.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeatData.Application.Products.Commands.CreateProduct
+{
+    public sealed class CreateProductHandler
+    {
+        private readonly IProductRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateProductHandler(IProductRepository repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result<Guid>> Handle(CreateProductCommand cmd, CancellationToken ct = default)
+        {
+            var skuExists = await _repository.ExistsBySkuAsync(cmd.SKU, ct);
+            if (skuExists) return Result<Guid>.Failure($"SKU '{cmd.SKU}' já existe.", "DUPLICATE_SKU");
+
+            var product = Product.Create(cmd.Name, cmd.Description, cmd.SKU, cmd.WeightGrams, cmd.CategoryId);
+
+            await _repository.AddAsync(product, ct);
+            await _unitOfWork.SaveChangesAsync(ct);
+
+            return Result<Guid>.Success(product.Id);
+        }
+    }
+}
